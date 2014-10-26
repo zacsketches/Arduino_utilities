36620c96ddc499e4aa1d8ac164403583003320c4
Zac <zacstaples@mac.com>
2014-10-25 18:40:39 -0700

    completed m file to solve for transfer function
      - found a and k for a simplified model of the encoded
        pololu motor.  The time constant was variable depending
        on the voltage applied...which frustrated the crap out
        of me.  So I am going to use the mean time constant and
        K value in the operating band for the motor.

--- time_vs_theta/gen_v_t_curve.m
+++ time_vs_theta/gen_v_t_curve.m
@@ -27,38 +27,41 @@ end
 %data vector is one element shorten than t
 t_final = t;
 t_final(1) = [];
+min_time = min(t_final);
+t_final = t_final-min_time;
 
 %plot the raw data
-plot(t_final, v_motor)
-ylabel('Angular Velocity (cts/ms)')
-xlabel('Time (ms)')
-title('Raw Response to Step Input')
-
-'raw data figure drawn...press any key to continue'
-pause
+figure(1); 
+plot(t_final, v_motor);
+ylabel('Angular Velocity (cts/ms)');
+xlabel('Time (ms)');
+title('Raw Response to Step Input');
 
 %implement a moving average filter
 counts_to_smooth = 30;
 coeff30MA = ones(1,counts_to_smooth)/counts_to_smooth;
-avg30_v_motor = filter(coeff30MA, 1, v_motor);
-
-%plot the smoothed data
-plot(t_final, avg30_v_motor)
-ylabel('Angular Velocity (cts/ms)')
-xlabel('Time (ms)')
-title('Moving Average 30 Sample Response to Step Input')
+avg_dx = filter(coeff30MA,1,dx);
+avg_dt = filter(coeff30MA,1,dt);
+%compute the data for avg_v
+avg_v = zeros(size(dt));
+index = 0;
+for index = 1:size(avg_v)
+    avg_v(index) = avg_dx(index) / avg_dt(index);
+end
+%filter one more time
+final_v = filter(coeff30MA, 1, avg_v);
 
 %find the velocity that represents 63% rise
-tau_val = .63 * max(avg30_v_motor);
-[c, tau_index] = min(abs(avg30_v_motor - tau_val));
-tau_val = avg30_v_motor(tau_index); % Finds first one only!
+tau_val = .63 * max(final_v);
+[c, tau_index] = min(abs(final_v - tau_val));
+tau_val = final_v(tau_index); % Finds first one only!
 tau_val
 time_constant = t_final(tau_index)-t_final(1);
 time_constant
 
 %find a and K
 a = 1/time_constant;
-forced_response = max(avg30_v_motor);
+forced_response = max(final_v);
 K = a * forced_response;
 a
 K
@@ -69,14 +72,56 @@ den = poly([-a]);
 G = tf(num, den);
 G
 
-%plot the time response
+%collect data for the step response of G
+[G_y, G_t] = step(G);
+
+%plot the smoothed data and the transfer function
+figure(2); 
+plot(t_final, final_v, G_t, G_y);
+ylabel('Angular Velocity (cts/ms)');
+xlabel('Time (ms)');
+title('\fontsize{14}Moving Average(N=30) Response to Step Input');
+legend('Smoothed data', 'Derived transfer function', 'Location', 'SouthEast');
+
+%show the time response to step input
 syms s;
 C = K/[s*(s+a)];
-c = ilaplace(C);
-c
+c_t = ilaplace(C);
+pretty(c_t)
 
 
+%try and scale the response to match a consistent gain
+Gain = 11.58 %from relatioship of K/a for all PWM runs
+scaled_v = final_v/Gain;
+scaled_t = t_final/Gain;
 
+figure(3);
+plot(scaled_t, scaled_v, G_t/Gain, G_y/Gain);
+ylabel('Angular Velocity (cts/ms)');
+xlabel('Time (ms)');
+title('\fontsize{14}Scaled Response to Step Input');
+legend('Smoothed data', 'Derived transfer function', 'Location', 'SouthEast');
+
+%find the velocity that represents 63% rise
+tau_val = .63 * max(scaled_v);
+[c, tau_index] = min(abs(scaled_v - tau_val));
+tau_val = scaled_v(tau_index); % Finds first one only!
+tau_val
+time_constant = scaled_t(tau_index);
+time_constant
+
+%find a and K
+a = 1/time_constant;
+forced_response = max(scaled_v);
+K = a * forced_response;
+a
+K
+
+%generate the transfer function
+num = K;
+den = poly([-a]);
+G = tf(num, den);
+G
 
 
 
